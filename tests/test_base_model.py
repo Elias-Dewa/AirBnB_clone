5f036ef4a9e426be033987dee3fc010b6b114e49
Elias Dewa Ahmed <54449260+Elias-Dewa@users.noreply.github.com>
2022-11-25 13:13:54 -0800

    AirBnB_clone project: create a test model folder and test for base_model file

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,15 +1,83 @@
 #!/usr/bin/python3
+from datetime import datetime
+import unittest
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key,
-          type(my_model_json[key]), my_model_json[key]))
+
+class TestBaseClass(unittest.TestCase):
+    """Representation of a set of tests for base model class
+    """
+
+    def test_type_of_id(self):
+        """Test to check the type of id
+        """
+        model = BaseModel()
+        self.assertTrue(type(model.id) == str)
+
+    def test_type_of_datetime(self):
+        """Test to check the type of created_at and updated_at
+        """
+        model = BaseModel()
+        self.assertTrue(type(model.created_at) == datetime)
+        self.assertTrue(type(model.updated_at) == datetime)
+
+    def test_str(self):
+        """Test to check the __str__ method
+        """
+        model = BaseModel()
+        self.assertEqual(model.__str__(), "[" + model.__class__.__name__ + "]"
+                         " (" + model.id + ") " + str(model.__dict__))
+
+    def test_uuid_generation(self):
+        """Test to check the generation of uuid
+        """
+        model1 = BaseModel()
+        model2 = BaseModel()
+        model3 = BaseModel()
+        self.assertNotTrue(model1.id == model2.id)
+        self.assertNotTrue(model2.id == model3.id)
+        self.assertNotTrue(model3.id == model1.id)
+
+    def test_to_dict(self):
+        """Test to check to_dict method
+        """
+        model = BaseModel()
+        my_model = model.to_dict()
+        self.assertTrue(type(my_model["created_at"] == str))
+        self.assertTrue(type(my_model["updated_at"] == str))
+        self.assertTrue(type(model.created_at) == datetime)
+        self.assertTrue(type(model.updated_at) == datetime)
+        self.assertEqual(my_model["created_at"], model.created_at.isoformat())
+        self.assertEqual(my_model["updated_at"], model.updated_at.isoformat())
+
+    def test_none_dict(self):
+        """Test to check for None dict
+        """
+        model = BaseModel(None)
+        self.assertTrue(type(model.id) == str)
+        self.assertTrue(type(model.created_at) == datetime)
+        self.assertTrue(type(model.updated_at) == datetime)
+
+    def test_kwargs_with_dict(self):
+        """Test to check when kwargs not empty
+        """
+        my_model = BaseModel()
+        my_model_json = my_model.to_dict()
+        my_new_model = BaseModel(**my_model_json)
+        self.assertEqual(my_model_json, my_new_model.to_dict())
+        self.assertTrue(type(my_new_model.id) == str)
+        self.assertTrue(type(my_new_model.created_at) == datetime)
+        self.assertTrue(type(my_new_model.updated_at) == datetime)
+
+    def test_kwargs_with_emp_dict(self):
+        """Test to check when kwargs is empty
+        """
+        my_dict = {}
+        my_model = BaseModel(**my_dict)
+        self.assertTrue(type(my_model.id) == str)
+        self.assertTrue(type(my_model.created_at) == datetime)
+        self.assertTrue(type(my_model.updated_at) == datetime)
+
+
+if __name__ == '__main__':
+    unittest.main()
