defabbbd3983176f549f1c2e39d93217dfc291b0
Amanuel2000 <amanuelf44@gmail.com>
2022-11-27 12:53:51 +0300

    updating tests and fixing bugs

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
-"""Unittest module for the City Class.
-"""
+"""Unittest module for the City Class."""
 
 import unittest
 from datetime import datetime
@@ -11,19 +9,30 @@ import re
 import json
 from models.engine.file_storage import FileStorage
 import os
-import pep8
 from models import storage
 from models.base_model import BaseModel
 
 
 class TestCity(unittest.TestCase):
+
     """Test Cases for the City class."""
 
     def setUp(self):
         """Sets up test methods."""
         pass
 
-    def test_city_instantiation(self):
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
+
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_8_instantiation(self):
         """Tests instantiation of City class."""
 
         b = City()
@@ -31,21 +40,13 @@ class TestCity(unittest.TestCase):
         self.assertIsInstance(b, City)
         self.assertTrue(issubclass(type(b), BaseModel))
 
-    def test_city_attributes(self):
+    def test_8_attributes(self):
         """Tests the attributes of City class."""
         attributes = storage.attributes()["City"]
         o = City()
         for k, v in attributes.items():
             self.assertTrue(hasattr(o, k))
             self.assertEqual(type(getattr(o, k, None)), v)
-
-    def test_pep8(self):
-        """Test to check pycodestyle
-        """
-        py_code_style = pep8.StyleGuide(quiet=True)
-        check = py_code_style.check_files(
-            ['models/user.py', 'tests/test_models/test_user.py'])
-        self.assertEqual(check.total_errors, 0, "Errors found")
 
 
 if __name__ == "__main__":
